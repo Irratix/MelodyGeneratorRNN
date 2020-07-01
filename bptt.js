e9d3536dbd63601b858e456b3973f359a374de6d
Simon <simonsimonsimon3995@gmail.com>
2020-07-01 22:46:30 +0200

    why do I keep forgetting to add files

---
+++ bptt.js
@@ -0,0 +1,10 @@
+//train a network on a dataset
+function train(network, data) {
+	//for each melody in the dataset
+	for (melody of data) {
+		//for each timestep (minus 1) in the melody
+		for (let i=1; i<melody.length; i++) {
+			//TODO
+		}
+	}
+}
